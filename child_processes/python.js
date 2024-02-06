7ca386e28a68774334da95145711ee7d85f75e18
Jagjit singh <jagjitsingh.btcivil20@pec.edu.in>
2024-02-06 11:43:08 +0530

    bug: handle error logs in child process

--- child_processes/python.js
+++ child_processes/python.js
@@ -3,5 +3,21 @@ const { MONGOURI } = require('../src/config/server-config');
 
 const pythonProcess = spawn('python', ['search.py', MONGOURI]);
 pythonProcess.stdin.setDefaultEncoding('utf-8');
+pythonProcess.stderr.on('data', (data) => {
+    console.error(`Python stderr: ${data}`);
+});
+
+pythonProcess.on('close', (code) => {
+    if (code !== 0) {
+        console.error(`Python process exited with code ${code}`);
+    } else {
+        console.log('Python process completed successfully');
+    }
+});
+
+pythonProcess.on('error', (err) => {
+    console.error('Error occurred while spawning Python process:', err);
+});
+console.log("Python process started");
 
 module.exports = pythonProcess;
