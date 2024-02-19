92725cc5c04266fb2dd2dca0fef159e8e0b870af
Jagjit singh <jagjitsingh.btcivil20@pec.edu.in>
2024-02-20 03:44:07 +0530

    refactor: remove coverphoto in schema blog

--- src/models/blog.js
+++ src/models/blog.js
@@ -4,7 +4,6 @@ const blogSchema = new mongoose.Schema({
     heading: { type: String, required: true },
     subHeading: { type: String, required: true },
     content: { type: String, required: true },
-    coverPhoto: { type: String, required: true }
 }, { timestamps: true });
 
 const Blog = new mongoose.model('Blog', blogSchema);
